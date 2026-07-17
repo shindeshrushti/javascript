ec1a3d131453c6d6828f548df5eb81d5e391b991
Shrushti Shinde <shrushtishinde262@gmail.com>
2026-07-17 08:48:58 +0000

    sum.js code

---
+++ javascript/sum.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+            let num1=number(prompt("enter first number"));
+            let num2=number(prompt("enter second number"));
+            let sum=num1+num2;
+            console.log("sum:"+sum);
+            
