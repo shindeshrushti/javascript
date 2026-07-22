9f3de6eca3bffa3271835a7c38ccee85e7d8529c
Shrushti Shinde <shrushtishinde262@gmail.com>
2026-07-22 08:26:35 +0000

    largest number.js

---
+++ 22_largest_number.js
@@ -0,0 +1,11 @@
+let a = 25;
+let b = 40;
+let c = 15;
+
+if (a > b && a > c) {
+    console.log("Largest =", a);
+} else if (b > c) {
+    console.log("Largest =", b);
+} else {
+    console.log("Largest =", c);
+}
