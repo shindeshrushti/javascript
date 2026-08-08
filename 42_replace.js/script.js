c6607e8380e78dd65773e56318c565118c649dbf
Shrushti Shinde <shrushtishinde262@gmail.com>
2026-08-08 11:41:50 +0000

    replace code

---
+++ 42_replace.js/script.js
@@ -0,0 +1,5 @@
+let str = prompt("Enter a string:");
+let oldWord = prompt("Enter word to replace:");
+let newWord = prompt("Enter new word:");
+
+document.write("Result = " + str.replace(oldWord, newWord));
